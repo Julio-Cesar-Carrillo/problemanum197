39aedcddd6c17b9d7eb13d6d2f1161491522a9ef
unknown <Julio-Cesar-Carrillo>
2023-10-04 20:21:26 +0200

    modificacion del readme, palabras a desencriptar

--- README.md
+++ README.md
@@ -1,44 +1,22 @@
 # problemanum197
 Ejercicio del problemanum197
 
-
-ENCRIPTACION 2 A 1 (x'' -> x')
-Inicializamos un array con las vocales
-Transformamos el mensaje encriptado a un array
-Inicializamos dos arrays vacios para guardar los caracteres pares e impares
-Recorremos el array y vamos almacenando los valores invertidos secuencialmente
-Invertimos los valores para que se pongan 
-Transformamos y fusionamos los dos arrays en un unico string
-
-ENCRIPTACION 1 A ORIGINAL (x' -> x)
-Inicializamos dos arrays
-Verificamos si el input de msge1 (x') esta vacia
-Recorremos el string msge1 (x')
-    Comprobamos si el caracter es una vocal o no
-    Recorremos el array msge1 (x') hasta encontrar una vocal, si lo encuentra no lo almacenando
-        En el caso de que no encuentre una vocal al final del array, se sale del while.
-     Invertimos los caracteres especulares para que se posicionen correctamente.
-    Fusionamos los dos arrays en un unico
-    Metemos en el array la vocal faltante
-    Limpiamos el array para la siguiente cadena de texto
-Transformamos el array en un string
-
-x'': Aauirnedleiua nBo
-x''->x' => AurelianoB uednia
-x'->x => Aureliano Buendia
-
-x'': E. .n.ualn cnhuag aMda  rle
-x''->x' => E nul nugad rel aM ahcna...
-x'->x => En un lugar de la Mancha...
-
-x'': aueoi
-x''->x' => aeiou
-x'->x => aeiou
-
-x'': BnodJo s, dBneam
-x''->x' => BoJ ,dnameB sodn
-x'->x => Bond, James Bond
-
-x'': dnany
-x''->x' => daynn
-x'->x => danny
+x'': Aauirnedleiua nBo <br>
+x''->x' => AurelianoB uednia <br>
+x'->x => Aureliano Buendia <br>
+<br>
+x'': E. .n.ualn cnhuag aMda  rle <br>
+x''->x' => E nul nugad rel aM ahcna... <br>
+x'->x => En un lugar de la Mancha... <br>
+<br>
+x'': aueoi <br>
+x''->x' => aeiou <br>
+x'->x => aeiou <br>
+<br>
+x'': BnodJo s, dBneam <br>
+x''->x' => BoJ ,dnameB sodn <br>
+x'->x => Bond, James Bond <br>
+<br>
+x'': dnany <br>
+x''->x' => daynn <br>
+x'->x => danny <br>
--- main.js
+++ main.js
@@ -1,40 +1,60 @@
 function procesarPalabra(){
+    // Recogemos la palabra a desencriptar
     var palabra = document.getElementById('palabra').value;
     // Creamos dos variables vacias
     var impar = "";
     var par = "";
     // Creamos un contador
     var contador=0;
-    // 
+    // Recogemos los dos resultados finales
     var primero = document.getElementById('resultado');
     var segundo = document.getElementById('resultado2');
+    // Mediante un for recoremos la palabra a desencriptar
     for (i = 0; i < palabra.length; i = i+1) {
+        // Creamos un array para cada palabra
         var letraActual = palabra[i];
+        // Incrementamos el contador
         contador = contador+1;
+        // Comparamos si el número del contador es par o no
         if (contador % 2 === 0) {
+            // Metemos la letra que se encuentre en el la posicion del array par
             par = letraActual+par;
         }
         else{
+            // juntamos la letra letra actual por la que se encuentra en el array
             impar = impar+letraActual;
         }
     }
+    // Juntamos las dos palabras
     var nueva = impar + par;
+    // Recogemos el segundo resultado y lo metemos en la constante frase
     primero.value = nueva;
     const frase = nueva;
+    // Creamos una constantes con las vocales
     const vocales = ['a', 'e', 'i', 'o', 'u'];
+    // Mediante un array separamos cada letra de la palabra
     const frase_array = frase.split('');
+    // Creamos un variable y un array vacio
     let resu = '';
     let reverso = [];
+    // Mediante un foreach recoremos cada palabra dentro del array
     frase_array.forEach((letra) => {
+        // Comprobamos que la letra no sea una vocal, tambien transformamos la letra en minusculas
         if (!vocales.includes(letra.toLowerCase())) {
+            // Metemos en un array la letra
             reverso.push(letra);
         } else {
+            // Si es una vocal la mandamos al final del array sin ningun tipo de separacion
             resu += reverso.reverse().join('');
+            // Metemos la vocal en la variable resu
             resu += letra;
+            // Limpiamos el array para la siguiente letra
             reverso = [];
         }
     })
+    // Comprovamos que reverso no este vacio 
     if (reverso != []) {
+        // Si no esta vacio la letra se ira al inicio de todo
         resu += reverso.reverse().join('')
     }
     segundo.value = resu;
